c7620246e51cdef197554d83a6c428e0111149b3
burakzaferozcan <ozcanburakzafer@gmail.com>
2024-04-01 00:04:21 +0300

    update

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,20 +1,31 @@
+import { Metadata } from "next";
+import { ReactNode } from "react";
+
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 
-import React, { ReactNode } from "react";
+export const metadata: Metadata = {
+  title: "MEET",
+  description: "Video calling app",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
-function HomeLayout({ children }: { children: ReactNode }) {
+const RootLayout = ({ children }: Readonly<{ children: ReactNode }>) => {
   return (
     <main className="relative">
       <Navbar />
+
       <div className="flex">
         <Sidebar />
-        <section className="flex min-h-screen  flex-1 flex-col px6 pb-6 pt-28 max-md:pb-14 sm:px-14">
+
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
         </section>
       </div>
     </main>
   );
-}
+};
 
-export default HomeLayout;
+export default RootLayout;
